5ffc58c8c7b48f0d9c7a1223186b1ff5f994dab3
Matt Jacobson <matthew.jacobson@grafana.com>
2023-10-08 14:32:04 -0400

    Greatly simplify am config migration
    Previously, we needed to know the migrated receivers to know which labels to
    attach to an alert rule.
    
    This change makes it so that we can create the am config without knowledge of
    the migrated alerts and vice-versa. This will be important for future work.
    
    This moves from a single `__contact__` label with an array of receiver names
    combined with regex-based route matching to multiple `__contact_{uid}__` labels
    and a simple equality-based route matching.
    
    Also removes some of the hlper structs around legacymodels.Alert.

--- pkg/services/ngalert/migration/models.go
+++ pkg/services/ngalert/migration/models.go
@@ -10,7 +10,6 @@ import (
 	"github.com/grafana/grafana/pkg/services/folder"
 	migmodels "github.com/grafana/grafana/pkg/services/ngalert/migration/models"
 	migrationStore "github.com/grafana/grafana/pkg/services/ngalert/migration/store"
-	"github.com/grafana/grafana/pkg/services/ngalert/models"
 	"github.com/grafana/grafana/pkg/services/ngalert/store"
 	"github.com/grafana/grafana/pkg/services/secrets"
 	"github.com/grafana/grafana/pkg/setting"
@@ -74,11 +73,6 @@ func (om *OrgMigration) AlertTitleDeduplicator(folderUID string) Deduplicator {
 		}
 	}
 	return om.alertRuleTitleDedup[folderUID]
-}
-
-type AlertPair struct {
-	AlertRule *models.AlertRule
-	DashAlert *migrationStore.DashAlert
 }
 
 // Deduplicator is a wrapper around map[string]struct{} and util.GenerateShortUID() which aims help maintain and generate
--- pkg/services/ngalert/migration/store/testing.go
+++ pkg/services/ngalert/migration/store/testing.go
@@ -73,6 +73,8 @@ func NewTestMigrationStore(t *testing.T, sqlStore *sqlstore.SQLStore, cfg *setti
 	err = acSvc.RegisterFixedRoles(context.Background())
 	require.NoError(t, err)
 
+	legacyAlertStore := legacyalerting.ProvideAlertStore(sqlStore, cache, cfg, nil, features)
+
 	return &migrationStore{
 		log:                            &logtest.Fake{},
 		cfg:                            cfg,
@@ -85,6 +87,7 @@ func NewTestMigrationStore(t *testing.T, sqlStore *sqlstore.SQLStore, cfg *setti
 		folderPermissions:              folderPermissions,
 		dashboardPermissions:           dashboardPermissions,
 		orgService:                     orgService,
+		legacyAlertStore:               legacyAlertStore,
 		legacyAlertNotificationService: legacyalerting.ProvideService(sqlStore, encryptionservice.SetupTestService(t), nil),
 	}
 }
